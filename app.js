43975c4df8c3df06e70b20543e3393cac202898a
Caio Willian <cwillian40@hotmail.com>
2020-08-06 23:40:48 -0300

    [#02] Nodemailer Config

--- app.js
+++ app.js
@@ -3,10 +3,33 @@ const nodemailer = require('nodemailer')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {    
-    res.send('Home')
+const user = "you_email_server@email.com"
+const passord = "123456"
+
+app.get('/', (req, res) => {
+  res.send('Home')
+})
+
+app.get('/send', (req, res) => {
+  const transporter = nodemailer.createTransport({
+    host: "smtp.server.com",
+    port: 587,
+    auth: {user, passord}
+  })
+
+  transporter.sendMail({
+    from: user,
+    to: user,
+    replyTo: "person@email.com",
+    subject: "Hello, Welcome!",
+    html: "Hello, Thanks for subscribe in my website!"
+  }).then((emitter) => {
+    res.send(emitter)
+  }).catch((error) => {
+    res.send(error)
+  })
 })
 
 app.listen(port, ()=> {
-    console.log(`Runnin on port ${port}`)
+  console.log(`Runnin on port ${port}`)
 })
